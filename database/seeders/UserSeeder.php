ba78c70e4225ad593a5367be21970a82e29b8399
Mateus Juvencio <mateus.juvencio@axxindata.com>
2023-08-26 14:58:04 -0300

    feat: criando seeder de usuario para adicionar varios usuarios por lote automaticamente

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $quantidadeNecessariaDeUsuarios = 100000;
+        $quantidadeCriadaPorLote = 5000;
+        $quantidadeDelotes = $quantidadeNecessariaDeUsuarios / $quantidadeCriadaPorLote;
+
+        for ($i = 0; $i < $quantidadeDelotes; $i++) {
+            User::factory()->times($quantidadeCriadaPorLote)->create();
+            $this->command->info("Lote " . ($i + 1) . " criado.");
+        }
+    }
+}
