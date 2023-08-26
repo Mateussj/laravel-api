d4d37d5e8bc43e0715f0fa67cb5988ce3c4a102c
Mateus Juvencio <mateus.juvencio@axxindata.com>
2023-08-26 16:28:48 -0300

    feat: adicionando testes ao controlador de usuarios

---
+++ tests/Feature/Controllers/UsusarioControllerTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UsusarioControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+
+       public function testcheandoSeIndexRetornaOsUsuariosCorretamente(){
+              User::factory()->count(5)->create();
+              $response = $this->get('/crud/users');
+              $response->assertStatus(200);
+              $response->assertJson(['data' => []]);
+              $response->assertJsonCount(5, 'data');
+              $response->assertJsonStructure([
+                     'data' => [
+                         '*' => [
+                             'id',
+                             'nome',
+                             'sobrenome',
+                             'telefone',
+                             'email',
+                             'created_at',
+                             'updated_at'
+                            ]
+                     ],
+                     'current_page',
+                     'first_page_url',
+                     'from',
+                     'last_page',
+                     'last_page_url',
+                     'links',
+                     'next_page_url',
+                     'path',
+                     "per_page",
+                     "prev_page_url",
+                     "to",
+                     "total"
+                 ]);
+       }
+
+       public function testChecandoSeShowRetornaUmUsuario(){
+              $user = User::factory()->count(1)->create();
+              
+              $response = $this->get("/crud/users/{$user[0]->id}");
+              $response->assertStatus(200);
+              $response->assertJsonStructure(
+                     [
+                            "nome",
+                            "sobrenome",
+                            "telefone",
+                            "email",
+                            "created_at",
+                            "updated_at"
+                     ]);
+       }
+
+       public function testChecandoSeUpdateAtualizaUmUsuario(){
+              $user = User::factory()->count(1)->create();
+              $new = [
+                     'nome' => "Name Teste", 
+                     'email' => "Email@Teste.com", 
+                     'telefone' => "988888555"
+              ];
+
+              $response = $this->put("/crud/users/{$user[0]->id}", $new);
+              $response->assertStatus(200);
+              $response->assertJson(
+                     [
+                            'message' => 'Usuário atualizado com sucesso.',
+                            'data' => $new
+                     ]);
+
+       }
+
+       public function testChecandoSeDeleteExcluiUmUsuario(){
+              $user = User::factory()->count(1)->create();
+
+              $response = $this->delete("/crud/users/{$user[0]->id}");
+              $response->assertStatus(200);
+              $response->assertJson(
+                     [
+                            'message' => 'Usuário excluído com sucesso.',
+                     ]);
+
+       }
+
+}
