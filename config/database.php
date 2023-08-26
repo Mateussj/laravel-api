87117ff6cbb10a471308e906360ed78b6500b8e2
Mateus Juvencio <mateus.juvencio@axxindata.com>
2023-08-26 16:26:33 -0300

    fix: adicionando configuração para rodar testes em um banco secundario

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'testes' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL_TESTE'),
+            'host' => env('DB_HOST_TESTE', '127.0.0.1'),
+            'port' => env('DB_PORT_TESTE', '3306'),
+            'database' => env('DB_DATABASE_TESTE', 'forge'),
+            'username' => env('DB_USERNAME_TESTE', 'forge'),
+            'password' => env('DB_PASSWORD_TESTE', ''),
+            'unix_socket' => env('DB_SOCKET_TESTE', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
